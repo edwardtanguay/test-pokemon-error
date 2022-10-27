5e8a8ad441c67ede747831b841811a456b81725c
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-27 22:32:30 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.scss'
+import { useState, useEffect } from 'react';
+import './App.scss';
+
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ivysaur';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Info Site</h1>
-		<p>Welcome to this site.</p>
-    </div>
-  )
+	const [pokemonObj, setPokemonObj] = useState({});
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(apiUrl);
+			const result = await response.json();
+			const _pokemonObj = result.sprites.other['official-artwork'];
+			console.log(_pokemonObj);
+			setPokemonObj(_pokemonObj);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Pokemon Test Site</h1>
+			<p>{pokemonObj.front_default}</p>
+			<img src={pokemonObj.front_default} />
+		</div>
+	);
 }
 
 export default App;
